9f2617de03d7484a080cbac6f3d7493c81e5b7c3
Kai Kodama <kai.kodama@nts.ricoh.co.jp>
2016-12-22 17:13:19 +0900

    Fix issue that cannot lgtm for lgtm.in own certificate authority

--- bots/lgtm.js
+++ bots/lgtm.js
@@ -11,6 +11,7 @@ module.exports = {
         "Cache-Control" : "no-cache",
         "Accept":"application/json"
       },
+      strictSSL: false,
     }, ( err, response, body ) => {
       const message = `[![Looks good to ${ req.body[ "user_name" ] }!](${ JSON.parse( body ).imageUrl } "Looks good to ${ req.body[ "user_name" ] }!")](${ JSON.parse( body ).imageUrl })`;
       console.log( message );
