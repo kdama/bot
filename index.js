8dc6b19fb40a11cb63a9b814c5c85e97c4cfe528
Kai Kodama <kai.kodama@nts.ricoh.co.jp>
2016-12-22 17:39:49 +0900

    Add lgtm-plain bot for Slack

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 1337; // heroku sets process.env.PORT
 
 const omikuji = require( "./bots/omikuji" );
 const lgtm = require( "./bots/lgtm" );
+const lgtmPlain = require( "./bots/lgtm-plain" );
 const zoi = require( "./bots/zoi" );
 
 createApp( {
@@ -13,6 +14,7 @@ createApp( {
   bots: {
     "/omikuji": omikuji,
     "/lgtm": lgtm,
+    "/lgtm-plain": lgtmPlain,
     "/zoi": zoi,
   },
 } );
